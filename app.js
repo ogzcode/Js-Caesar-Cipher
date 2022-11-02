ecf5c008f5370493a8f9926d66c2749ef0441bd2
ogzCode <oguzguc44@gmail.com>
2022-11-02 19:56:17 +0300

    Encryption algorithm optimized

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 function range(start, stop){
-    //A-Z arasındaki harfleri veren fonksiyon
     const result = [];
     for (let idx = start.charCodeAt(0), end = stop.charCodeAt(0); idx <=end; ++idx){
         result.push(String.fromCharCode(idx));
@@ -7,59 +6,50 @@ function range(start, stop){
     return result;
 }
 
-function findIndex(char){
-    //Verilen harfin alfabedeki konumunu verir
-    for (let c of chars){
-        if (char == c){
-            return chars.indexOf(c);
-        }
-    }
-}
-
-function sifrele(value){
-    let result = [];
-    for (let i = 0; i < value.length; i++){
-        let index = findIndex(value[i]);
+function encrypt(value){
+    let result = "";
+    const alphabet = range("A", "Z");
+    for (let i of value){
+        let index = alphabet.indexOf(i);
         let newIndex = index + 3;
-        result[i] = chars[Math.abs(newIndex % chars.length)];
+        result += alphabet[Math.abs(newIndex % alphabet.length)];
     }
-
-    return result.join(" ");
+    return result;
 }
 
-function sifreCoz(value){
-    console.log(value);
-    let result = [];
-    for (let i = 0; i < value.length; i++){
-        let index = findIndex(value[i]);
+function decrypt(value){
+    let result = "";
+    const alphabet = range("A", "Z");
+    for (let i of value){
+        let index = alphabet.indexOf(i);
         let newIndex = index - 3;
         if(newIndex < 0){
-            newIndex = chars.length + newIndex;
+            newIndex = alphabet.length + newIndex;
         }
-        result[i] = chars[Math.abs(newIndex % chars.length)];
+        result += alphabet[Math.abs(newIndex % alphabet.length)];
     }
-
-    return result.join(" ");
+    return result;
 }
 
+const encryptInput = document.getElementById("encrypt-input");
+const encryptBtn = document.getElementById("encrypt-btn");
+const cipherText = document.getElementById("cipher-text");
 
-const inputOne = document.getElementById("input-1");
-const inputTwo = document.getElementById("input-2");
-const btn1 = document.getElementById("btn-1");
-const btn2 = document.getElementById("btn-2");
-const chars = range('A','Z').join("");
+const decryptInput = document.getElementById("decrypt-input");
+const decryptBtn = document.getElementById("decrypt-btn");
+const normalText = document.getElementById("normal-text");
 
 
-btn1.addEventListener("click", () => {
-    //Girilen değeri şifreler
-    document.getElementById("span-1").innerText = sifrele(inputOne.value.toUpperCase());
-    inputOne.value = "";
+encryptBtn.addEventListener("click", () => {
+    cipherText.innerText = encrypt(encryptInput.value.toUpperCase());
+    cipherText.style.color = "#9b1d22";
+    encryptInput.value = "";
 });
 
-btn2.addEventListener("click", () => {
-    //Girilen değerin şifresini çözer
-    document.getElementById("span-2").innerText = sifreCoz(inputTwo.value.toUpperCase());
-    inputTwo.value = "";
+decryptBtn.addEventListener("click", () => {
+    normalText.innerText = decrypt(decryptInput.value.toUpperCase());
+    normalText.style.color = "#9b1d22";
+    decryptInput.value = "";
 });
 
 
